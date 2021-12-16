289a0e3cf02e9d3a87168a74676397a1a96d2d80
JIRARAJ <92936610+JIRARAJ@users.noreply.github.com>
2021-12-16 19:25:16 +0530

    Add files via upload

---
+++ if else.py
@@ -0,0 +1,6 @@
+name = input("Enter the name:")
+age = int(input("enter the age:"))
+if age>= 18:
+    print(name,"age is",age,"Eligible for vote.")
+else:
+    print(name,"age is",age,"not eligible to vote.")
